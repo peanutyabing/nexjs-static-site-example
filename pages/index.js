8df4ba8aeb30155269347da2678495faa202916c
peanutyabing <sherry.liuyabing@gmail.com>
2023-04-09 20:52:46 +0800

    Add blog post links to index page

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import Head from "next/head";
 import Layout, { siteTitle } from "../components/layout.js";
 import utilStyles from "../styles/utils.module.css";
 import Link from "next/link.js";
+import Date from "../components/date.js";
 import { getSortedPostsData } from "../lib/posts";
 
 export async function getStaticProps() {
@@ -32,11 +33,11 @@ export default function Home({ allPostsData }) {
         <ul className={utilStyles.list}>
           {allPostsData.map(({ id, date, title }) => (
             <li className={utilStyles.listItem} key={id}>
-              {title}
+              <Link href={`/posts/${id}`}>{title}</Link>
               <br />
-              {id}
-              <br />
-              {date}
+              <small className={utilStyles.lightText}>
+                <Date dateString={date} />
+              </small>
             </li>
           ))}
         </ul>
